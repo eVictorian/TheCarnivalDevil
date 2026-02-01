ea57cb00fc4d878c5ae110195b4806ebb9877c7b
Ashly Fox <ashyboi2056@gmail.com>
2026-02-01 01:14:08 +0000

    DEV BUILD 1

--- Assets/[1] Scripts/[2] Creature/CREATURE_Murder.cs
+++ Assets/[1] Scripts/[2] Creature/CREATURE_Murder.cs
@@ -5,6 +5,8 @@ public class CREATURE_Murder : MonoBehaviour
 {
     [SerializeField] private soDATA_GameEvent blackoutStartedGameEvent;
 
+    static bool RULE_MURDER_OnlyOneKillPerCheck = true;
+
     void Awake(){ Setup(); }
     void Setup(){ blackoutStartedGameEvent.RegisterListener(OnBlackoutCheckForMurder); }
 
@@ -16,15 +18,26 @@ public class CREATURE_Murder : MonoBehaviour
 
         List<ENTITY> roomOccupants = new List<ENTITY>(myRoom.GetOccupants());
 
-        if (roomOccupants.Count == 2)
+        if (!QueryMurder(roomOccupants)){ return; }
+        
+        foreach (ENTITY entity in roomOccupants)
         {
-            foreach (ENTITY entity in roomOccupants)
-            {
-                if (entity is ENTITY_Creature){}
-                if (entity is ENTITY_Guest){ KillGuest(entity as ENTITY_Guest); }
-                if (entity is ENTITY_Player){ KillPlayer(entity as ENTITY_Player); }
-            }
+            if (entity is ENTITY_Creature){}
+            if (entity is ENTITY_Guest){ KillGuest(entity as ENTITY_Guest); if (RULE_MURDER_OnlyOneKillPerCheck){ break; }}
+            if (entity is ENTITY_Player){ KillPlayer(entity as ENTITY_Player); if (RULE_MURDER_OnlyOneKillPerCheck){ break; }}
         }
+    }
+
+    bool QueryMurder(List<ENTITY> occupants)
+    {
+        foreach (ENTITY entity in occupants)
+        {
+            if (entity is ENTITY_Creature){}
+            if (entity is ENTITY_Guest){}
+            if (entity is ENTITY_Player){ return false; }
+        }
+
+        return true;
     }
 
     void KillPlayer(ENTITY_Player player){ player.Die(); }
--- Assets/[1] Scripts/[5] UX + Audio/AudioSelfDelete.cs
+++ Assets/[1] Scripts/[5] UX + Audio/AudioSelfDelete.cs
@@ -2,17 +2,11 @@ using UnityEngine;
 
 public class AudioSelfDelete : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    void Awake()
     {
         Invoke("KillYourSelfNow", GetComponent<AudioSource>().clip.length);
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
     void KillYourSelfNow()
     {
         Destroy(gameObject);
